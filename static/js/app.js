519f56f8f7cb1fba3d722ba101583b0e0d00c050
Vero Castillo <77795761+verocastillo@users.noreply.github.com>
2021-07-23 22:00:37 -0500

    Gauge chart works.

--- static/js/app.js
+++ static/js/app.js
@@ -22,18 +22,22 @@ function runEnter() {
   if (inputYear === "2016"){
     document.getElementById("subtitle").textContent="Performance Analysis: 2016";
     linechart(inputYear);
+    gaugechart(inputYear);
   }
   else if (inputYear === "2017"){
     document.getElementById("subtitle").textContent="Performance Analysis: 2017";
     linechart(inputYear);
+    gaugechart(inputYear);
   }
   else if (inputYear === "2018"){
     document.getElementById("subtitle").textContent="Performance Analysis: 2018";
     linechart(inputYear);
+    gaugechart(inputYear);
   }
   else {
     document.getElementById("subtitle").textContent="Performance Analysis: All Years";
     linechart(inputYear);
+    gaugechart(inputYear);
   }
 }
 
@@ -214,6 +218,167 @@ function linechart(inputYear) {
         margin: { l: 110, r: 10, t: 110, b: 50 }
       };
       Plotly.newPlot('scatter', dataline, layoutline);
+    })
+  }
+}
+
+// Create gauge charts
+function gaugechart(inputYear) {
+  // Change value according to element
+  if (inputYear === "2016"){
+    d3.json("flask-database/json-data/maindata2016.json").then((data)=> {
+      var gaugedata = data;
+      var reviews = []
+      for (var i = 0; i < gaugedata.length; ++i) {
+        reviews.push(gaugedata[i].review_score)
+      }
+      //console.log(reviews)
+      reviewsum = reviews.reduce((a, b) => a + b, 0)
+      reviewnum = reviews.length
+      avgreview = reviewsum/reviewnum
+      var gaugedata = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: avgreview,
+        title: { text: "<b>Average Order Review Score</b><br> 2016" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [null, 5] },
+          bar: { color: "royalblue" },
+          steps: [
+            { range: [null, 1], color: "lightsteelblue" },
+            { range: [1, 2], color: "powderblue" },
+            { range: [2, 3], color: "lightblue" },
+            { range: [3, 4], color: "skyblue" },
+            { range: [4, 5], color: "lightskyblue" }
+          ],
+          threshold: {
+            line: { color: "blue", width: 4 },
+            thickness: 0.75,
+            value: 490
+            }
+        }
+      }
+      ];
+      var gaugelayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gaugedata, gaugelayout);
+    })
+  }
+  else if (inputYear === "2017"){
+    d3.json("flask-database/json-data/maindata2017.json").then((data)=> {
+      var gaugedata = data;
+      var reviews = []
+      for (var i = 0; i < gaugedata.length; ++i) {
+        reviews.push(gaugedata[i].review_score)
+      }
+      //console.log(reviews)
+      reviewsum = reviews.reduce((a, b) => a + b, 0)
+      reviewnum = reviews.length
+      avgreview = reviewsum/reviewnum
+      var gaugedata = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: avgreview,
+        title: { text: "<b>Average Order Review Score</b><br> 2017" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [null, 5] },
+          bar: { color: "royalblue" },
+          steps: [
+            { range: [null, 1], color: "lightsteelblue" },
+            { range: [1, 2], color: "powderblue" },
+            { range: [2, 3], color: "lightblue" },
+            { range: [3, 4], color: "skyblue" },
+            { range: [4, 5], color: "lightskyblue" }
+          ],
+          threshold: {
+            line: { color: "blue", width: 4 },
+            thickness: 0.75,
+            value: 490
+            }
+        }
+      }
+      ];
+      var gaugelayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gaugedata, gaugelayout);
+    })
+  }
+  else if (inputYear === "2018"){
+    d3.json("flask-database/json-data/maindata2018.json").then((data)=> {
+      var gaugedata = data;
+      var reviews = []
+      for (var i = 0; i < gaugedata.length; ++i) {
+        reviews.push(gaugedata[i].review_score)
+      }
+      //console.log(reviews)
+      reviewsum = reviews.reduce((a, b) => a + b, 0)
+      reviewnum = reviews.length
+      avgreview = reviewsum/reviewnum
+      var gaugedata = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: avgreview,
+        title: { text: "<b>Average Order Review Score</b><br> 2018" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [null, 5] },
+          bar: { color: "royalblue" },
+          steps: [
+            { range: [null, 1], color: "lightsteelblue" },
+            { range: [1, 2], color: "powderblue" },
+            { range: [2, 3], color: "lightblue" },
+            { range: [3, 4], color: "skyblue" },
+            { range: [4, 5], color: "lightskyblue" }
+          ],
+          threshold: {
+            line: { color: "blue", width: 4 },
+            thickness: 0.75,
+            value: 490
+            }
+        }
+      }
+      ];
+      var gaugelayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gaugedata, gaugelayout);
+    })
+  }
+  else {
+    d3.json("flask-database/json-data/maindata.json").then((data)=> {
+      var gaugedata = data;
+      var reviews = []
+      for (var i = 0; i < gaugedata.length; ++i) {
+        reviews.push(gaugedata[i].review_score)
+      }
+      //console.log(reviews)
+      reviewsum = reviews.reduce((a, b) => a + b, 0)
+      reviewnum = reviews.length
+      avgreview = reviewsum/reviewnum
+      var gaugedata = [{
+        domain: { x: [0, 1], y: [0, 1] },
+        value: avgreview,
+        title: { text: "<b>Average Order Review Score</b><br> All Years" },
+        type: "indicator",
+        mode: "gauge+number+delta",
+        gauge: {
+          axis: { range: [null, 5] },
+          bar: { color: "royalblue" },
+          steps: [
+            { range: [null, 1], color: "lightsteelblue" },
+            { range: [1, 2], color: "powderblue" },
+            { range: [2, 3], color: "lightblue" },
+            { range: [3, 4], color: "skyblue" },
+            { range: [4, 5], color: "lightskyblue" }
+          ],
+          threshold: {
+            line: { color: "blue", width: 4 },
+            thickness: 0.75,
+            value: 490
+            }
+        }
+      }
+      ];
+      var gaugelayout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
+    Plotly.newPlot('gauge', gaugedata, gaugelayout);
     })
   }
 }
