905837a62005a6d297bfa02c4a4a5eaf3b0e5103
SoniaSu <frutos.rojos17@gmail.com>
2021-07-24 01:06:54 -0500

    html

--- static/js/logic.js
+++ static/js/logic.js
@@ -15,6 +15,13 @@ function createMap(year,category) {
   
   
   // Define streetmap and darkmap layers
+  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "dark-v10",
+    accessToken: API_KEY
+   });
+  
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -24,12 +31,7 @@ function createMap(year,category) {
     accessToken: API_KEY
   });
 
-  var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "dark-v10",
-    accessToken: API_KEY
-   });
+  
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
@@ -45,16 +47,16 @@ function createMap(year,category) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load
    myMap = L.map("map", {
     center: [
-      -23.5489, -46.6388
-      //-7.6,-37.8
+      //-23.5489, -46.6388
+      -10,-55
     ],
-    zoom: 10,
+    zoom: 3,
     layers: [streetmap]
   });
 
   
 
-  d3.csv("static/data/dataset.csv").then(function(geoData) {     
+  d3.csv("static/data/dataset2.csv").then(function(geoData) {     
     geoData = geoData.filter(filtroAnio);
     
     geoData.forEach(element =>      
@@ -90,18 +92,18 @@ function createMap(year,category) {
 
 }
 function getColor(value){
-    if(value <= 100000)
-        return 'DarkSeaGreen';
-    else if(value > 100000 && value <=200000)
-        return 'green';
-    else if(value > 200000 && value <=300000)
-        return 'yellow';
-    else if(value > 300000 && value <=400000)
-        return 'orange';
-    else if(value > 400000 && value <=500000)
-        return 'coral';
-    else if(value > 500000)
-        return 'crimson';
+    if(value <= 500)
+        return 'Aqua';
+    else if(value > 500 && value <=5000)
+        return 'Fuchsia';
+    else if(value > 5000 && value <=50000)
+        return 'Blue';
+    else if(value > 50000 && value <=80000)
+        return 'Navy';
+    else if(value > 80000 && value <=100000)
+        return 'Teal';
+    else if(value > 100000)
+        return 'Purple';
 }
 
 
